8193b40aa4301460a1df66a607a472963e583ae2
Fawn <sarahmargaret@ymail.com>
2023-05-30 17:34:07 -0400

    added port number code

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+
