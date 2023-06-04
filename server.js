69db5e8ae490ffbcbc8c9517bf5eb0c2fee2cc3a
Fawn <sarahmargaret@ymail.com>
2023-06-04 14:09:14 -0400

    added routes for notes page and homepage

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const api = require("./routes/index.js");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -8,4 +9,18 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use("/", api);
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+
+  app.listen(PORT, () => {
+    console.log(
+      `Note saver is listening at http://localhost:${PORT}`
+    );
+  });
