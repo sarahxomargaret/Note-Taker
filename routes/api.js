08f6d019d73eca260dd168f5f55982db561d250f
Fawn <sarahmargaret@ymail.com>
2023-08-22 18:25:00 -0400

    added GET and POST

---
+++ routes/api.js
@@ -0,0 +1,26 @@
+const apiRouter = require('express').Router();
+const fs = require('fs');
+const storedNotes = require('../db/db.json');
+const noteID = require('../helpers/noteID');
+let noteData = storedNotes;
+
+// GET route for retrieving all the notes
+apiRouter.get('/notes', (req, res) => res.json(noteData));
+
+// POST route for saving a new note
+apiRouter.post("/notes", function (req, res) {
+    var newNote = (req.body);
+    console.log(newNote);
+
+    storedNotes.push(newNote);
+    req.body.index = storedNotes.length;
+    res.json(newNote);
+
+    fs.writeFile(storedNotes, newNote, "utf8", function (err) {
+      if (err) throw err;
+    
+    });
+
+});
+
+module.exports = apiRouter;
